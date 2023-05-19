8e61047e62faa2d554fd65ce264541094cad63e2
Doan Duc <dukinfotech@DucDoan>
2023-05-19 23:18:47 +0900

    setup .env for frontend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const fs = require('fs');
+const path = require('path');
 
+// Create a symlink to .env file
+const envFilePath = path.join(path.dirname(process.cwd()), '.env');
+const envLocalFilePath = path.join(process.cwd(), '.env.local')
+fs.symlink(envFilePath, envLocalFilePath, err => {
+  if (err.code === 'EEXIST') {}
+});
+
+const nextConfig = {}
 module.exports = nextConfig
